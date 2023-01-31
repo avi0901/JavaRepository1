ecdc1e43b7c37017ff71ce5bee9ba54afd6ed08d
Avinash Kumar <avikumar100210@gmail.com>
2023-01-31 15:08:17 +0530

    New One

--- GitPro/src/Demo.java
+++ GitPro/src/Demo.java
@@ -3,7 +3,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello Doston");
+		System.out.println("Avi here");
 	}
 
 }
